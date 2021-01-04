378bb8f00ce3b73aba3ee9d0996c8f7d4cb59913
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2021-01-04 12:40:47 -0500

    commit tree a6f34fbca7b43cbcf706a029c30d45fc116e29d0

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -30,14 +30,25 @@ const posterMap = {
 export default class MovieCard extends Component {
 
   render() {
+    console.log(this.props)
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront poster={posterMap[this.props.poster]} />
+        <CardBack  
+          title={this.props.title}
+					IMDBRating={this.props.IMDBRating}
+					genres={this.props.genres}/>
       </div>
     )
   }
+}
+
+MovieCard.defaultProps = {
+	title: 'Unknown',
+	IMDBRating: null,
+	genres: ['No Genre(s) Found'],
+  poster: 'default'
 }
 
 // Don't forget your default props!
